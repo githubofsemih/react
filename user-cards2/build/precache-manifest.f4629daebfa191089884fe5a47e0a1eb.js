9cd23d2c57bd32c37b94f7379506859cecc9b212
Semih Arslan <91609912+githubofsemih@users.noreply.github.com>
2021-10-04 01:18:48 +0300

    Add files via upload

---
+++ user-cards2/build/precache-manifest.f4629daebfa191089884fe5a47e0a1eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20e8e0f4a724cb44d8af3c61e7b5857f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "387a034a9d07df201562",
+    "url": "/static/css/main.a42cb9d1.chunk.css"
+  },
+  {
+    "revision": "7affddea0dd383ae417b",
+    "url": "/static/js/2.53b9c8f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.53b9c8f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "387a034a9d07df201562",
+    "url": "/static/js/main.b1b6d41e.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
